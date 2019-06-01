d17317a8452dd48df61d5a21da7deecc3ff4f50f
BEUMGOO <captain_kbg@naver.com>
2019-06-02 00:02:26 +0900

    setting server

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # catch-mind
+
+Realtime Drawing Game built with SocketIO, Gulp and Node
+
+
+## To Do:
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [ ] SocketIO
---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
