439051dec267eaa1ed04174026e45e8a00dccf03
BEUMGOO <captain_kbg@naver.com>
2019-06-14 23:44:41 +0900

    user login

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
 import logger from "morgan";
+import socketController from "./socketController";
 
 const PORT = 4000;
 const app = express();
@@ -25,14 +26,4 @@ const io = socketIO.listen(server);
 //Socketはページがなく、コネクトしかない
 //eventしかない
 //connectionのエントリーポイント
-io.on("connection", socket => {
-  socket.on("newMessage", ({ message }) => {
-    socket.broadcast.emit("messageNotif", {
-      message,
-      nickname: socket.nickname || "Anon"
-    });
-  });
-  socket.on("setNickname", ({ nickname }) => {
-    socket.nickname = nickname;
-  });
-});
+io.on("connection", socket => socketController(socket));
