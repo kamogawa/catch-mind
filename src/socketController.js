6c71e84d420774ea79d5ee0dfb432e2758e10486
BEUMGOO <captain_kbg@naver.com>
2019-06-17 23:31:06 +0900

    Refactoring

--- src/socketController.js
+++ src/socketController.js
@@ -4,6 +4,7 @@ const socketController = socket => {
   socket.on(events.setNickname, ({ nickname }) => {
     console.log(nickname);
     socket.nickname = nickname;
+    socket.broadcast.emit(events.newUser, { nickname });
   });
 };
 
