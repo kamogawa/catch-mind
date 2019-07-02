0c701322de3418278c7461a1f0455984e47f1bcd
BEUMGOO <captain_kbg@naver.com>
2019-07-02 23:10:59 +0900

    paiter css

--- src/socketController.js
+++ src/socketController.js
@@ -4,7 +4,6 @@ const socketController = socket => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
 
   socket.on(events.setNickname, ({ nickname }) => {
-    console.log(nickname);
     socket.nickname = nickname;
     broadcast(events.newUser, { nickname });
   });
@@ -13,8 +12,17 @@ const socketController = socket => {
     broadcast(events.disconnected, { nickname: socket.nickname });
   });
 
-  socket.on(events.sendMsg, ({ message }) => {
-    broadcast(events.newMsg, { message, nickname: socket.nickname });
+  socket.on(events.sendMsg, ({ message }) =>
+    broadcast(events.newMsg, { message, nickname: socket.nickname })
+  );
+
+  socket.on(events.beginPath, ({ x, y }) =>
+    broadcast(events.beganPath, { x, y })
+  );
+
+  socket.on(events.strokePath, ({ x, y }) => {
+    broadcast(events.strokedPath, { x, y });
+    console.log(x, y);
   });
 };
 
